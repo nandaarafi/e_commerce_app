7e44f39172f40154c6c29f86b7f2bed8fab18368
nandaarafi <nanda.rafi614@gmail.com>
2024-02-09 19:49:56 +0700

    add onboarding_screen android and IOS lauchimage error

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce_app/core/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:e_commerce_app/src/on_boarding/presentation/screen/onboarding.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,10 +12,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: EcomTheme.lightTheme,
       darkTheme: EcomTheme.darkTheme,
+      home: const OnBoardingScreen(),
     );
   }
 }
